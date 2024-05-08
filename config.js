3d97e92790fc0ac7a035cbd57dc0bfd1d296d44f
kingibrahimtech <161978665+kingibrahimtech@users.noreply.github.com>
2024-05-08 13:28:48 +0300

    closed

--- config.js
+++ config.js
@@ -38,7 +38,7 @@ global.save_status_from = process.env.SAVE_STATUS_FROM || "";
 global.read_status_from = process.env.READ_STATUS_FROM || "";
 
 global.api_smd = "https://api-smd-1.vercel.app";
-global.scan = "https://black-panther-scanner-871df7205958.herokuapp.com/";
+global.scan = "https://ibrahim-tech-qr-1-2-1.onrender.com/";
 
 global.SESSION_ID =
   process.env.SESSION_ID ||
