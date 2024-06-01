fccd153f59afe7467cb8f3179cabd145586493db
MBUVI DAVID <147725809+cheekydavy@users.noreply.github.com>
2024-06-01 07:46:24 +0300

    Update config.js

--- config.js
+++ config.js
@@ -13,13 +13,13 @@ global.allowJids = process.env.ALLOW_JID || "null";
 global.blockJids = process.env.BLOCK_JID || "null";
 global.DATABASE_URL = process.env.DATABASE_URL || "";
 global.timezone = process.env.TZ || process.env.TIME_ZONE || "Africa/Nairobi";
-global.github = process.env.GITHUB || "https://github.com/kingibrahimtech/Black-panther-md-x";
-global.gurl = process.env.GURL || "https://whatsapp.com/channel/0029VaZuGSxEawdxZK9CzM0Y";
-global.website = process.env.GURL || "https://whatsapp.com/channel/0029VaZuGSxEawdxZK9CzM0Y";
-global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://telegra.ph/file/7260c0965d835f26fad48.jpg";
-global.devs = "254710772666";
-global.sudo = process.env.SUDO || "254710772666";
-global.owner = process.env.OWNER_NUMBER || "254710772666";
+global.github = process.env.GITHUB || "https://github.com/cheekydavy/savage-mxd";
+global.gurl = process.env.GURL || "https://whatsapp.com/channel/0029VaPZWbY1iUxVVRIIOm0D";
+global.website = process.env.GURL || "https://whatsapp.com/channel/0029VaPZWbY1iUxVVRIIOm0D";
+global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://telegra.ph/file/a7ca8d4777fa8ad977795.jpg";
+global.devs = "254746440595";
+global.sudo = process.env.SUDO || "254746440595";
+global.owner = process.env.OWNER_NUMBER || "254746440595";
 global.style = process.env.STYLE || "1";
 global.gdbye = process.env.GOODBYE || "false";
 global.wlcm = process.env.WELCOME || "false";
@@ -27,7 +27,7 @@ global.warncount = process.env.WARN_COUNT || 3;
 global.disablepm = process.env.DISABLE_PM || "false";
 global.disablegroup = process.env.DISABLE_GROUPS || "false",
 global.MsgsInLog = process.env.MSGS_IN_LOG || "true";
-global.userImages = process.env.USER_IMAGES || "https://telegra.ph/file/ec8ce4af1d8fa3271e26a.jpg,https://telegra.ph/file/ec059aa5db556bbd951c6.jpg";
+global.userImages = process.env.USER_IMAGES || "https://telegra.ph/file/a7ca8d4777fa8ad977795.jpg,https://telegra.ph/file/a7ca8d4777fa8ad977795.jpg";
 global.waPresence = process.env.WAPRESENCE || "online";
 global.readcmds = process.env.READ_COMMAND || "false";
 global.readmessage = process.env.READ_MESSAGE || "false";
@@ -48,11 +48,11 @@ module.exports = {
   HANDLERS: process.env.PREFIX || ".",
   BRANCH: process.env.BRANCH || "main",
   VERSION: process.env.VERSION || "1.0.0",
-  caption: process.env.CAPTION || "𝐵𝐿𝐴𝐶𝐾 𝑃𝐴𝑁𝑇𝐻𝐸𝑅 𝘟5",
-  author: process.env.PACK_AUTHER || "𝑃𝐴𝑁𝑇𝐻𝐸𝑅",
+  caption: process.env.CAPTION || "SAVAGE MD",
+  author: process.env.PACK_AUTHER || "SAVAGE",
   packname: process.env.PACK_NAME || "🐯",
-  botname: process.env.BOT_NAME || "𝐁𝐋𝐀𝐂𝐊 𝐏𝐀𝐍𝐓𝐇𝐄𝐑 𝐌𝐃",
-  ownername: process.env.OWNER_NAME || "𝑰𝒃𝒓𝒂𝒉𝒊𝒎 𝑨𝒅𝒂𝒎𝒔",
+  botname: process.env.BOT_NAME || "SAVAGE MD",
+  ownername: process.env.OWNER_NAME || "MBUVI TECH",
   errorChat: process.env.ERROR_CHAT || "254710772666",
   KOYEB_API: process.env.KOYEB_API || "false",
   REMOVE_BG_KEY: process.env.REMOVE_BG_KEY || "",
